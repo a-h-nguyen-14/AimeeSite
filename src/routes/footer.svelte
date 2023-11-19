4f3ac308843b49143aea3d755798998e7ef6689b
a-h-nguyen-14 <a.h.nguyen.14@icloud.com>
2023-11-19 14:31:33 +1100

    footer change

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -10,12 +10,12 @@
 </script>
 
 <div class="bg-autism-blue">
-<Footer class="bg-autism-blue">
-	<FooterCopyright href="/" by="Adapted Judo" year={2023} />
-	<FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-		<FooterLink href='/bibliography'>References</FooterLink>
-		<FooterLink href=''>Partners</FooterLink>
-		<FooterLink href=''>Contact Email</FooterLink>
-	</FooterLinkGroup>
-</Footer>
+	<Footer class="!bg-autism-blue">
+		<FooterCopyright href="/" by="Adapted Judo" year={2023} />
+		<FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+			<FooterLink href='/bibliography'>References</FooterLink>
+			<FooterLink href=''>Partners</FooterLink>
+			<FooterLink href=''>Contact Email</FooterLink>
+		</FooterLinkGroup>
+	</Footer>
 </div>
